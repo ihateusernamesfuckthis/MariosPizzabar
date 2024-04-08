2ecc6b677a87d6b63830162136d31d9420753198
elisa236164 <s236164@dtu.dk>
2024-04-08 22:03:11 +0200

    pizzabarview

--- PizzaBarView.java
+++ PizzaBarView.java
@@ -1,104 +1,103 @@
 import java.util.*;
+
 public class PizzaBarView {
-    Private pizzaBarOrder pizzaBar;
-    public static void pizzaBarView(pizzaBarOrder pizzaBar){//konstruktør
-    this.pizzaBar = pizzaBar;
+    private PizzaBarOrder pizzaBar;
 
+    public PizzaBarView(PizzaBarOrder pizzaBar) {
+        this.pizzaBar = pizzaBar;
     }
-    public static void showMenu(){
-        System.out.print("Showing the menu");
-        for(pizzas pizza : pizzaMenu){
-            System.out.print(pizza.getName() + "-" + + pizza.getNumber + "-" + pizza.getPrice() + "kr")
 
+    public void showMenu() {
+        System.out.print("Showing the menu");
+        for(Pizza pizza : pizzaMenu) {
+            System.out.print(pizzaBar.getName() + "-" + pizzaBar.getNumber() + "-" + pizzaBar.getPrice() + "kr");
         }
     }
 
-    public static void takeOrder(){
+    public void takeOrder() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("what would you like to order?");
+        System.out.print("What is your name?");
+        String customerName = scan.nextLine();
+        System.out.println("What would you like to order?");
         String order = scan.nextLine();
         System.out.println("Anything else? y/n");
         String orderMore = scan.nextLine();
-        if (orderMore.equals("y")){
-            else{
-                //System.out.println("perfect!\n i will call you when your order is finished");
-            }
-            System.out.print("What is your name?");
-            String CustomerName = scan.nextLine();
+        if (orderMore.equals("y")) {
+            System.out.println("Perfect!\n" + customerName + " I will call you when your order is finished");
         }
-        System.out.println("perfect!\n " + CustomerName + " I will call you when your order is finished");
+        System.out.println("Perfect!\n" + customerName + " I will call you when your order is finished");
         pizzaBar.add(order, orderMore);
+        scan.close();
     }
 
-    public static void editOrder(){
+    public void editOrder() {
         Scanner scan = new Scanner(System.in);
-        booling running = true;
-        while(running) {
+        boolean running = true;
+        while (running) {
             System.out.print("What Would You Like to Change?");
             System.out.print("Enter 1: delete order");
             System.out.print("Enter 2: add order");
-           // System.out.print("Enter 3: change pick up time");
-            System.out.print("Enter 4: no changes")
+            System.out.print("Enter 3: cancel order");
+            System.out.print("Enter 4: no changes");
 
-            int choose = scan.input.nextInt();
-            switch (choose){
+            int choose = scan.nextInt();
+            switch (choose) {
                 case 1:
-                    //add en ordrer
+                    // Add en ordrer
                     System.out.println("What is your name?");
-                    String CustomerName = scan.nextLine(); // hvad hvis de ikke kan finde den i systemet
+                    String customerName = scan.nextLine();
                     System.out.println("Perfect. I found you in my system");
                     System.out.println("This is your order");
-                    ystem.out.print(pizza.order());
+                    System.out.print(pizza.order());
                     System.out.println("What would you like to add from your order");
                     String addMore = scan.nextLine();
                     pizzaBar.add(addMore);
-
                     break;
                 case 2:
-                    //slette en ordrer
+                    // Slet en ordrer
                     System.out.println("What is your name?");
-                    String CustomerName = scan.nextLine(); // hvad hvis de ikke kan finde den i systemet
+                    customerName = scan.nextLine();
                     System.out.println("Perfect. I found you in my system");
                     System.out.println("What would you like to remove from your order");
                     String removePizza = scan.nextLine();
                     pizzaBar.remove(removePizza);
-                    System.out.println("Your order looks like as you preffered");
-                    System.out.print(pizza.order()); //Mangler customer id number for verify
-
+                    System.out.println("Your order looks like as you preferred");
+                    System.out.print(pizza.order()); // mangler customer id number for verify
                     break;
                 case 3:
-                    //ændre afhentningstidpunktet?
+                    pizzaBar.clear();
+                    System.out.print("Your order has been cancelled");
                     break;
                 case 4:
-                    booling running = false;
-                    System.out.println("perfect!\n i will call you when your order is finished");
+                    running = false;
+                    System.out.println("Perfect!\n I will call you when your order is finished");
                     break;
                 default:
-                    System.out.println("perfect!\n i will call you when your order is finished");
+                    System.out.println("Perfect!\n I will call you when your order is finished");
             }
         }
-    }
-    public static void handleOrder(){
-        //fjernet fra listen
-        order.setCompleted(true); //ordren er færdig?
-        pizzaBar.remove(order);
-        System.out.print("Order" + orderId + "is now removed from the list");
+        scan.close();
     }
 
-    public static void getStatistick(){
+    public void handleOrder() {
+        // Fjernet fra listen
+        order.setCompleted(true); // Er ordren færdig?
+        pizzaBar.remove(order);
+        System.out.print("Order " + orderId + " is now removed from the list");
+    }
+
+    public void getStatistic() {
         int revenue = 0;
         int mostSoldPizzas = 0;
 
-        /*
-        for(orders order : pizzaBar.orders) {
-             mostSoldPizzas += order.isCompleted.size();
+        for (Order order : pizzaBar.orders) { //korrekt?
+            mostSoldPizzas += order.size();
         }
-        for(pizzaPrices pizzaPrice : order.getPizza)
-        revenue += pizza.getPrice();
+        for (PizzaPrices pizzaPrice : pizzaBar.getPizza()) {
+            revenue += pizzaPrice.getPrice();
+        }
 
-         */
-
-        System.out.print("The most sold pizza is " + mostSoldPizzas); //lav top 5
-        System.out.print("The total earnings " + revenue " kr");
+        System.out.print("The most sold pizza is " + mostSoldPizzas); // Lav top 5
+        System.out.print("The total earnings " + revenue + " kr");
     }
 }
