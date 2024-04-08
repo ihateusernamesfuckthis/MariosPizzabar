f1847354f3f93ebe3a0535c25922ce43bcf072ac
ihateusernamesfuckthis <pepe0003@kea.stud.dk>
2024-04-08 12:55:10 +0200

    test 2

--- MainPizzaBar.java
+++ MainPizzaBar.java
@@ -1,6 +1,5 @@
 public class MainPizzaBar {
     public static void main(String[] args) {
-        System.out.println("Hello world 2");
-        System.out.println("Hello world 3");
+        System.out.println("Hello world 1");
     }
 }
